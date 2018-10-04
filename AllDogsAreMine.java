5c4d54f624b833815d0175ba99ed24bbf8ea31e0
Kromany <43865676+Kromany@users.noreply.github.com>
2018-10-04 18:51:29 -0400

    Create AllDogsAreMine.java

---
+++ AllDogsAreMine.java
@@ -0,0 +1,19 @@
+public class FourLeggedPet
+{
+
+    String name;
+    int age;
+    String color;
+
+public FourLeggedPet(String name, int age, String color)
+{
+
+    this.name = name;
+    this.age = agw;
+    this.color = color;
+
+
+}
+
+
+}
